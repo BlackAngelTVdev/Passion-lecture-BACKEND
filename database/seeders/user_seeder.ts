795da57bf110de36380d61b70730ccea37bd4a0f
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-04-29 09:55:22 +0200

    fix(seeder) : correction d'un bug de duplication lors du seeder des user
    [6][DONE]

--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -6,16 +6,26 @@ export default class extends BaseSeeder {
     const users = [
       {
         username: 'Admin',
+        email: 'admin@example.com',
         password: 'Admin',
         admin: true,
       },
       {
         username: 'Test',
+        email: 'test@example.com',
         password: '1234',
         admin: false,
       },
     ]
+    for (const userPayload of users) {
+      const existingUser = await User.findBy('username', userPayload.username)
 
-    await User.createMany(users)
+      if (existingUser) {
+        existingUser.merge(userPayload)
+        await existingUser.save()
+      } else {
+        await User.create(userPayload)
+      }
+    }
   }
 }
