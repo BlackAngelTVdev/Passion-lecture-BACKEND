cd9b0bf95a3392dc35f4beb89a01a7e10301828a
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-05-05 16:40:52 +0200

    feat(validator) : création d'un validator pour le auth
    [38][DONE]

---
+++ app/validators/auth.ts
@@ -0,0 +1,28 @@
+import vine from '@vinejs/vine'
+
+/**
+ * Validateur pour l'inscription
+ */
+export const registerValidator = vine.compile(
+  vine.object({
+    username: vine.string().trim().minLength(3).maxLength(50).unique(async (db, value) => {
+      const user = await db.from('users').where('username', value).first()
+      return !user
+    }),
+    email: vine.string().email().normalizeEmail().unique(async (db, value) => {
+      const user = await db.from('users').where('email', value).first()
+      return !user
+    }),
+    password: vine.string().minLength(8)
+  })
+)
+
+/**
+ * Validateur pour la connexion
+ */
+export const loginValidator = vine.compile(
+  vine.object({
+    username: vine.string(),
+    password: vine.string(),
+  })
+)
