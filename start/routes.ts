3276c98ee517e84073ee0aa0de68092cb3466f1c
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-05-12 17:02:23 +0200

    feat(rate) : ajout des route de rate
    [14][DONE]

--- start/routes.ts
+++ start/routes.ts
@@ -5,6 +5,7 @@ import swaggerConfig from '#config/swagger'
 const LivresController = () => import('#controllers/livres_controller')
 const AuthController = () => import('#controllers/auth_controller')
 const CommentairesController = () => import('#controllers/commentaires_controller')
+const RatesController = () => import('#controllers/rates_controller')
 
 // Routes Swagger
 router.get('/swagger.json', async () => {
@@ -26,6 +27,12 @@ router.get('/', ({ response }) => {
 router.post('/login', [AuthController, 'login'])
 router.post('/register', [AuthController, 'register'])
 
+// Public routes for reading books and ratings
+router.get('/books', [LivresController, 'index'])
+router.get('/books/:id', [LivresController, 'show'])
+router.get('/books/:id/comments', [CommentairesController, 'index'])
+router.get('/books/:bookId/rates', [RatesController, 'index'])
+
 // Protected routes (require authentication)
 router
   .group(() => {
@@ -33,13 +40,22 @@ router
     router.post('/logout', [AuthController, 'logout'])
     router.get('/profile', [AuthController, 'profile'])
 
-    // Books routes
-  })
-  .middleware(() => import('#middleware/auth_middleware'))
+    // Books write operations
+    router.post('/books', [LivresController, 'store'])
+    router.put('/books/:id', [LivresController, 'update'])
+    router.delete('/books/:id', [LivresController, 'destroy'])
 
-router.resource('books', LivresController).apiOnly()
+
+  })
+  .middleware([() => import('#middleware/auth_middleware')])
+      // Comments operations
     router.post('/books/:id/comments', [CommentairesController, 'store'])
-    router.get('/books/:id/comments', [CommentairesController, 'index'])
     router.get('/books/:id/comments/:commentId', [CommentairesController, 'show'])
     router.put('/books/:id/comments/:commentId', [CommentairesController, 'update'])
     router.delete('/books/:id/comments/:commentId', [CommentairesController, 'destroy'])
+
+    // Rating routes
+    router.post('/books/:bookId/rates', [RatesController, 'store'])
+    router.get('/books/:bookId/rates/:id', [RatesController, 'show'])
+    router.put('/books/:bookId/rates/:id', [RatesController, 'update'])
+    router.delete('/books/:bookId/rates/:id', [RatesController, 'destroy'])
